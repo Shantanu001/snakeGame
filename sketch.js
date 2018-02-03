810dab423e76d40d0bf870766caf70d6db8a11bc
Shantanu Jaiswal <sjshantanu001@gmail.com>
2018-02-03 20:16:13 +0530

    Adding all the function

--- sketch.js
+++ sketch.js
@@ -1,18 +1,37 @@
 var scl =20;
 var s ;
+var food;
 function setup() {
   // put setup code here
      createCanvas(600,600);
     s= new Snake();
     frameRate(10);
+     //food = createVector(random(width),random(height));
+    picklocation();
 }
 
 function draw() {
   // put drawing code here
      background(0); 
+     if(s.eat(food))
+        {
+            picklocation();
+        }
+    s.die();
     s.update();
     s.show();
-    
+    fill('#00ff00');
+    rect(food.x,food.y,scl,scl);
+   
+}
+function mousePressed(){
+    s.total++;
+}
+function picklocation(){
+    let row = floor(width/scl);
+    let col = floor(height/scl);
+    food = createVector(floor(random(col)),floor(random(row)));
+    food.mult(scl);
 }
 
 
--- snake.js
+++ snake.js
@@ -3,6 +3,34 @@ function Snake(){
     this.y = 0;
     this.speedx = 1;
     this.speedy = 0;
+    this.total =0;
+    this.tail = [];
+    
+    //check if the snake dies
+    this.die = function() {
+        for(var i=0;i<this.tail.length;i++)
+            {
+                var pos = this.tail[i];
+                if(dist(this.x,this.y,pos.x,pos.y)<1)
+                    {
+                         alert ("GAME OVER " + "SCORE:- " +this.total); 
+                          this.total =0;
+                          this.tail = [];
+                    }
+            }
+        
+    }
+    
+    
+    
+    //check if snake eats the food
+    this.eat = function(food) {
+        //console.log("total");
+        let dis = dist(this.x,this.y,food.x,food.y);
+        if(dis<1){this.total+=1;return true;}
+        else {return false;}
+    }
+   
     
     //Update the direction next to be moved
     this.dn = function(x,y) {
@@ -12,16 +40,37 @@ function Snake(){
     
     //update the snake direction of moving from current position
     this.update = function() {
-      this.x = this.x + this.speedx*scl;
-    this.y = this.y + this.speedy*scl;     
+        if(this.total >= 1)
+        {this.tail[this.total-1] = createVector(this.x,this.y);}    
+             
+             for(var i =0 ;i<this.tail.length-1;i++)
+            {
+                this.tail[i] = this.tail[i+1];
+                //console.log("yes" + this.total );
+    
+            }
+        
+    this.x = this.x + this.speedx*scl;
+    this.y = this.y + this.speedy*scl;
+    
+    //Putting the constraint on the snake    
+    this.x = constrain (this.x,0,width-scl);
+    this.y = constrain (this.y,0,height-scl);
+        
     }
     
     //display the snake
     this.show = function() {
-         fill('#00ff00');
-         rect(this.x,this.y,20,20);
+         fill(255);
+        for(var i = 0 ; i < this.tail.length;i++)
+            {
+        
+                 rect(this.tail[i].x, this.tail[i].y ,scl,scl);
+            }
+         rect(this.x,this.y,scl,scl);
     }
-   
+    
+
    
     
 }
